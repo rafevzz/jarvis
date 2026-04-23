52afe208cbd5b0520bbe507303bc73420f3f3dba
mohammad rehan <qazir36@gmail.com>
2026-04-23 14:18:52 +0530

    hood design done

---
+++ www/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.text').textillate({
+        loop: true,
+        syce: true,
+        in:{
+            effect: "bounceIn",
+
+        },
+        out:{
+            effect: "bounceOut",
+            
+        },
+    })
+});
